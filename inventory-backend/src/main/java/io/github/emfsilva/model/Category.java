adafc8063c67f521f20fb8fa24e6369e87f68c4a
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-07-21 23:48:02 -0300

    creación model Category

---
+++ inventory-backend/src/main/java/io/github/emfsilva/model/Category.java
@@ -0,0 +1,20 @@
+package io.github.emfsilva.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+
+}
