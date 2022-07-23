6a7814e68a306ad284d214eda4d87f7b30bf6d72
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-07-22 23:03:01 -0300

    Implementacion search en la clase controller

---
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/controller/CategoryRestController.java
@@ -0,0 +1,26 @@
+package io.github.emfsilva.inventory.controller;
+
+import io.github.emfsilva.inventory.response.CategoryResponseRest;
+import io.github.emfsilva.inventory.services.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/category")
+public class CategoryRestController {
+
+    private final ICategoryService categoryService;
+
+    @Autowired
+    public CategoryRestController(ICategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<CategoryResponseRest> searchCategories() {
+        return categoryService.search();
+    }
+}
