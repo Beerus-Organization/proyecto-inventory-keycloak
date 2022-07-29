0dfdfb4735e2ef466d12765fae76c588e6300aed
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-07-28 22:22:49 -0300

    Obteniendo categorias en el componente

--- front-inventory/src/app/modules/category/components/category/category.component.ts
+++ front-inventory/src/app/modules/category/components/category/category.component.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from './../../../shared/services/category.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
+
   }
 
+  getCategories() {
+    this.categoryService.getCategories().subscribe({
+    next: (data:any) => {
+      console.log("respusta",data)
+    },
+    error: (error:any) => {
+      console.log("respusta", error)
+    }
+  })
+  }
 }
