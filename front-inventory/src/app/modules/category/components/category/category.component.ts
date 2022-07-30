7deb000c567199d65d1f32db4d2f4f858813c6d4
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-07-30 18:56:57 -0300

    Configurando tabla - ng-container

--- front-inventory/src/app/modules/category/components/category/category.component.ts
+++ front-inventory/src/app/modules/category/components/category/category.component.ts
@@ -1,5 +1,7 @@
 import { CategoryService } from './../../../shared/services/category.service';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-category',
@@ -12,17 +14,38 @@ export class CategoryComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCategories();
-
   }
+
+  displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
+  dataSource = new MatTableDataSource<CategoryElement>();
 
   getCategories() {
     this.categoryService.getCategories().subscribe({
     next: (data:any) => {
-      console.log("respusta",data)
+      console.log("respusta",data);
+      this.processCategoriesResponse(data);
     },
     error: (error:any) => {
       console.log("respusta", error)
       }
     })
   }
+
+  processCategoriesResponse(resp: any) {
+    const dataCategory: CategoryElement[] = [];
+    if( resp.metadata[0].code == "00") {
+      let listCategory = resp.categoryResponse.category;
+
+      listCategory.forEach((element: CategoryElement) => {
+        dataCategory.push(element);
+      });
+
+      this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
+    }
+  }
+}
+export interface CategoryElement {
+  description: string;
+  id: number;
+  name: string;
 }
