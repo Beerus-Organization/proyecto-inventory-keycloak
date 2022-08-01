4db53cc614aa35dfc96b613b362e31162df44e7a
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-07-31 22:13:48 -0300

    Manejo de formularios con FormGroup

--- front-inventory/src/app/modules/category/components/new-category/new-category.component.ts
+++ front-inventory/src/app/modules/category/components/new-category/new-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-category',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCategoryComponent implements OnInit {
 
-  constructor() { }
+  public categoryForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+
+      this.categoryForm = this.formBuilder.group( {
+        name: ['', Validators.required],
+        description: ['', Validators.required]
+      });
+   }
 
   ngOnInit(): void {
   }
