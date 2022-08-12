9220bf55291b562e21dd6242e813dda119ee9d96
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-08-12 08:17:43 -0300

    Estableciendo modelo Product - ManyToOne

--- inventory-backend/src/main/java/io/github/emfsilva/inventory/model/Product.java
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/model/Product.java
@@ -1,4 +1,30 @@
 package io.github.emfsilva.inventory.model;
 
-public class Product {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    private Category category;
+
+    @Column(name = "picture", length = 1000)
+    private byte[] picture;
+
 }
