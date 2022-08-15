8d33ac7484f52ed0d97ddae46ee633c4ef98ec36
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-08-14 21:10:16 -0300

    Implementacion Interface JpaRepository IProductDao

--- inventory-backend/src/main/java/io/github/emfsilva/inventory/dao/IProductDao.java
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/dao/IProductDao.java
@@ -1,4 +1,7 @@
 package io.github.emfsilva.inventory.dao;
 
-public interface IProductDao {
+import io.github.emfsilva.inventory.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductDao extends JpaRepository<Product, Long> {
 }
--- inventory-backend/src/main/java/io/github/emfsilva/inventory/response/ProductResponse.java
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/response/ProductResponse.java
@@ -1,4 +1,13 @@
 package io.github.emfsilva.inventory.response;
 
+import io.github.emfsilva.inventory.model.Product;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class ProductResponse {
+
+    List<Product> products;
+
 }
