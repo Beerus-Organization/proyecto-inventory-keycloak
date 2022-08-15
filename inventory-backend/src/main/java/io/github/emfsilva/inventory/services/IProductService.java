d2bf55d5691fa92e92c17a4692e427ba0b6b164f
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-08-15 20:56:33 -0300

    Declarando firma método save

--- inventory-backend/src/main/java/io/github/emfsilva/inventory/services/IProductService.java
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/services/IProductService.java
@@ -1,4 +1,11 @@
 package io.github.emfsilva.inventory.services;
 
+import io.github.emfsilva.inventory.model.Product;
+import io.github.emfsilva.inventory.response.ProductResponse;
+import io.github.emfsilva.inventory.response.rest.ProductResponseRest;
+import org.springframework.http.ResponseEntity;
+
 public interface IProductService {
+
+    ResponseEntity<ProductResponseRest> save(Product product, Long categoryId);
 }
--- inventory-backend/src/main/java/io/github/emfsilva/inventory/services/impl/ProductServiceImpl.java
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/services/impl/ProductServiceImpl.java
@@ -1,9 +1,22 @@
 package io.github.emfsilva.inventory.services.impl;
 
+import io.github.emfsilva.inventory.model.Product;
+import io.github.emfsilva.inventory.response.rest.ProductResponseRest;
 import io.github.emfsilva.inventory.services.IProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductServiceImpl implements IProductService {
 
+    @Override
+    public ResponseEntity<ProductResponseRest> save(Product product, Long categoryId) {
+        ProductResponseRest response = new ProductResponseRest();
+        List<Product> list = new ArrayList<>();
+
+        return null;
+    }
 }
