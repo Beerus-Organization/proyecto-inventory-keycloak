3249771db00dfbd4dc02ce32693ca75c87c05717
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-08-15 21:53:47 -0300

    Comprimiendo foto en base64

--- inventory-backend/src/main/java/io/github/emfsilva/inventory/controller/ProductRestController.java
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/controller/ProductRestController.java
@@ -1,7 +1,35 @@
 package io.github.emfsilva.inventory.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.github.emfsilva.inventory.model.Product;
+import io.github.emfsilva.inventory.response.rest.ProductResponseRest;
+import io.github.emfsilva.inventory.utils.Util;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
+@RequestMapping("/v1/product")
 public class ProductRestController {
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductResponseRest> save(
+            @RequestParam("picture")MultipartFile picture,
+            @RequestParam("name") String name,
+            @RequestParam("price") BigDecimal price,
+            @RequestParam("quantity") Integer quantity,
+            @RequestParam("categoryId") Long categoryID) throws IOException
+    {
+        Product product = new Product();
+        product.setName(name);
+        product.setQuantity(quantity);
+        product.setPrice(price);
+        product.setPicture(Util.compressZLib(picture.getBytes()));
+
+        return null;
+    }
+
 }
---
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/utils/Util.java
@@ -0,0 +1,58 @@
+package io.github.emfsilva.inventory.utils;
+
+import io.github.emfsilva.inventory.services.impl.ProductServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+public class Util {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Util.class);
+
+    // compress the image bytes before storing it in the database
+    public static byte[] compressZLib(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+        }
+        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+
+        return outputStream.toByteArray();
+    }
+
+    // uncompress the image bytes before returning it to the angular application
+    public static byte[] decompressZLib(byte[] data) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+
+        try {
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            outputStream.close();
+        } catch (IOException | DataFormatException e) {
+            LOGGER.info("Error to compress image  {}", e.getMessage());
+        }
+        return outputStream.toByteArray();
+    }
+
+
+}
