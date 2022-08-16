be3c98cf452a1a942912de57da93342784d18222
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-08-15 21:28:34 -0300

    Terminando implementacion servicio

--- inventory-backend/src/main/java/io/github/emfsilva/inventory/services/impl/ProductServiceImpl.java
+++ inventory-backend/src/main/java/io/github/emfsilva/inventory/services/impl/ProductServiceImpl.java
@@ -1,11 +1,14 @@
 package io.github.emfsilva.inventory.services.impl;
 
 import io.github.emfsilva.inventory.dao.ICategoryDao;
+import io.github.emfsilva.inventory.dao.IProductDao;
 import io.github.emfsilva.inventory.model.Category;
 import io.github.emfsilva.inventory.model.Product;
 import io.github.emfsilva.inventory.response.rest.ProductResponseRest;
 import io.github.emfsilva.inventory.response.rest.ResponseRest;
 import io.github.emfsilva.inventory.services.IProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,10 +20,14 @@ import java.util.Optional;
 @Service
 public class ProductServiceImpl implements IProductService {
 
-    private ICategoryDao categoryDao;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductServiceImpl.class);
 
-    public ProductServiceImpl(ICategoryDao categoryDao) {
+    private ICategoryDao categoryDao;
+    private IProductDao productDao;
+
+    public ProductServiceImpl(ICategoryDao categoryDao, IProductDao productDao) {
         this.categoryDao = categoryDao;
+        this.productDao = productDao;
     }
 
     @Override
@@ -34,13 +41,28 @@ public class ProductServiceImpl implements IProductService {
             if (category.isPresent()) {
                 product.setCategory(category.get());
             } else {
-                response.setMetadata("Request NOK", "-1", "Category association with product");
-                return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+                response.setMetadata("Request NOK", "-1", "Category no associated with product");
+                return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+            }
+
+            // save the product
+            Product productSaved = productDao.save(product);
+
+            if(productSaved != null) {
+                list.add(productSaved);
+                response.getProduct().setProducts(list);
+                response.setMetadata("Request OK", "00", "Product saved");
+            } else {
+                response.setMetadata("Request NOK", "-1", "Product cant be save");
+                return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
             }
         }
         catch (Exception e) {
-
+            response.setMetadata("Request NOK", "-1", "Error to save product");
+            LOGGER.info("Error to save product {}", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return null;
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
